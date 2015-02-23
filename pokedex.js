2e7a4f05fb39f23536d4f581244daa7360142c4e
Dominik Yakoubek <domyak1@nths219.org>
2015-02-23 09:10:51 -0600

    Create pokedex.js

---
+++ pokedex.js
@@ -0,0 +1,949 @@
+exports.BattlePokedex = {
+//Information about each Bammon is located here
+simorgh: {
+	num: -801,
+	species: Simorgh,
+	types: ["Fire", "Flying"],
+	genderRatio: {
+		M: 0.25,
+		F: 0.75
+	},
+	baseStats: {
+		hp: 80,
+		atk: 110,
+		def: 90,
+		spa: 60,
+		spd: 90,
+		spe: 100
+	},
+	abilities: {
+		0: "Enraged"
+	},
+	heightm: 0.0
+	weightkg: 0.0
+	color: "Red",
+	eggGroups: ["Undiscovered"],
+	otherFormes: ["simorghmega"]
+},
+simorghmega: {
+	num: -801,
+	species: "Simorgh-Mega",
+	baseSpecies: "Simorgh",
+	forme: "Mega",
+	formeLetter: "M",
+	types: ["Fire", "Flying"],
+	genderRatio: {
+		M: 0.25,
+		F: 0.75
+	},
+	baseStats: {
+		hp: 80,
+		atk: 100,
+		def: 105,
+		spa: 145,
+		spd: 105,
+		spe: 95
+	},
+	abilities: {
+		0: "Mystic Veil"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Red"
+	eggGroups: ["Undiscovered"]
+},
+flytrap: {
+	num: -803,
+	species: "Flytrap",
+	types: ["Grass", "Poison"],
+	baseStats: {
+		hp: 65,
+		atk: 130,
+		def: 115,
+		spa: 85,
+		spd: 105,
+		spe: 50
+	},
+	abilities: {
+		0: "Enchanted Ivy"
+	},
+	heightm: 0.00,
+	weightkg: 0.00,
+	color: "color",
+	eggGroups: ["Undiscovered"],
+	otherFormes: ["flytrapmega"]
+},
+flytrapmega: {
+	num: -803,
+	species: "Flytrap-Mega",
+	baseSpecies: "Flytrap",
+	forme: "Mega",
+	formeLetter: "M",
+	types: ["Grass", "Poison"],
+	baseStats: {
+		hp: 65,
+		atk: 170,
+		def: 129,
+		spa: 87,
+		spd: 124,
+		spe: 54
+	},
+	abilities: {
+		0: "Planting Roots"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "color",
+	eggGroups: ["Undiscovered"]
+},
+name: {
+	num: -804,
+	species: "Name",
+	types: ["Normal", "Flying"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 46,
+		atk: 92,
+		def: 101,
+		spa: 94,
+		spd: 102,
+		spe: 115
+	},
+	abilities: {
+		0: "Unknown"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Green",
+	eggGroups: ["Undiscovered"]
+},
+pandoze: {
+	num: -805,
+	species: "Pandoze",
+	types: ["Normal"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 140,
+		atk: 57,
+		def: 56,
+		spa: 80,
+		spd: 105,
+		spe: 12
+	},
+	abilities: {
+		0: "Sleeping Strength"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "color",
+	eggGroups: ["Undiscovered"]
+},
+apaflite: {
+	num: -806,
+	species: "Apaflite",
+	types: ["Normal"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 75,
+		atk: 140,
+		def: 200,
+		spa: 45,
+		spd: 90,
+		spd: 55
+	},
+	abilities: {
+		0: "Resolve"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+},
+dendurus: {
+	num: -807,
+	species: "Dendurus",
+	types: ["Rock", "Ground"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 106,
+		atk: 112,
+		def: 66,
+		spa: 40,
+		spd: 109,
+		spe: 47
+	},
+	abilities: {
+		0: "Desert",
+		H: "Jagged Hide"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Brown",
+	eggGroups: ["Undiscovered"]
+},
+bandirto: {
+	num: -808,
+	species: "Bandirto",
+	types: ["Ground", "Dark"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 40,
+		atk: 110,
+		def: 90,
+		spa: 105,
+		spd: 90,
+		spe: 50
+	},
+	abilities: {
+		0: "Quick Blows",
+		H: "Pain Eater"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+},
+tiflectum: {
+	num: -810,
+	species: "Tiflectum",
+	types: ["Steel"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 119,
+		atk: 45,
+		def: 123,
+		spa: 76,
+		spd: 126,
+		spe: 38
+	},
+	abilities: {
+		0: "Endurance",
+		H: "Mystic Mirror"
+	},
+	heightm: 1.8,
+	weightkg: 189.4,
+	color: "Gray",
+	eggGroups: ["Undiscovered"]
+},
+sinder: {
+	num: -811,
+	species: "Sinder",
+	types: ["Fire"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 70,
+		atk: 65,
+		def: 70,
+		spa: 45,
+		spd: 140,
+		spe: 55
+	},
+	abilities: {
+		0: "Dauntless"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Red",
+	eggGroups: ["Undiscovered"]
+},
+springfest: {
+	num: -813,
+	species: "Springfest",
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 90,
+		atk: 70,
+		def: 100,
+		spa: 120,
+		spd: 100,
+		spe: 95
+	},
+	abilities: {
+		0: "Meadow",
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Green",
+	eggGroups: ["Undiscovered"]
+},
+mirrage: {
+	num: -814,
+	species: "Mirrage",
+	types: ["Ghost"],
+	gender: "N",
+	baseStats: {
+		hp: 90,
+		atk: 60,
+		def: 80,
+		spa: 125,
+		spd: 80,
+		spe: 110
+	},
+	abilities: {
+		0: "Magic Mirror"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "White",
+	eggGroups: ["Undiscovered"]
+},
+skulprit: {
+	num: -815,
+	species: "Skulprit",
+	types: ["Ghost"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 160,
+		atk: 112,
+		def: 68,
+		spa: 39,
+		spd: 93,
+		spe: 52
+	},
+	abilities: {
+		0: "Refraction",
+		H: "Spook"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+},
+swelter: {
+	num: -818,
+	species: "Swelter",
+	types: ["Ice", "Fire"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 90,
+		atk: 40,
+		def: 115,
+		spa: 95,
+		spd: 120,
+		spe: 85
+	},
+	abilities: {
+		0: "Resolve",
+		H: "Fire Proof"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "White",
+	eggGroups: ["Undiscovered"]
+},
+tundear: {
+	num: -819,
+	species: "Tundear",
+	types: ["Ice", "Ground"],
+	baseStats: {
+		hp: 87,
+		atk: 99,
+		def: 113,
+		spa: 54,
+		spd: 102,
+		spe: 68
+	},
+	abilities: {
+		0: "Muddy Armor",
+		H: "Endurance"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "color",
+	eggGroups: ["Undiscovered"]
+},
+slimurk: {
+	num: -820,
+	species: "Slimurk",
+	types: ["Poison", "Dark"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 60,
+		atk: 125,
+		def: 57,
+		spa: 100,
+		spd: 57,
+		spe: 101
+	},
+	abilities: {
+		0: "Pollutant",
+		H: "Kleptomaniac"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Purple",
+	eggGroups: ["Undiscovered"]
+},
+sluborac: {
+	num: -821,
+	species: "Sluborac",
+	types: ["Poison", "Fighting"],
+	genderRatio: {
+		M: 0.8,
+		F: 0.2
+	},
+	baseStats: {
+		hp: 100,
+		atk: 120,
+		def: 90,
+		spa: 60,
+		spd: 90,
+		spe: 75
+	},
+	abilities: {
+		0: "Hazardous",
+		H: "Dauntless"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Blue",
+	eggGroups: ["Undiscovered"]
+},
+ogre: {
+	num: -822,
+	species: "Ogre",
+	types: ["Water", "Poison"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 90,
+		atk: 100,
+		def: 90,
+		spa: 50,
+		spd: 80,
+		spe: 70
+	},
+	abilities: {
+		0: "Contamination"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Blue",
+	eggGroups: ["Undiscovered"]
+},
+name: {
+	num: -823,
+	species: "Name",
+	types: ["Water", "Dragon"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 50,
+		atk: 105,
+		def: 20,
+		spa: 20,
+		spd: 20,
+		spe: 140
+	},
+	abilities: {
+		0: "Second Chance",
+		H: "Immunity"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+},
+name: {
+	num: -824,
+	species: "Name",
+	types: ["Ice"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 115,
+		atk: 60,
+		def: 100,
+		spa: 90,
+		spd: 115,
+		spe: 40
+	},
+	abilities: {
+		0: "Glacier"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+},
+cychill: {
+	num: -825,
+	species: "Cychill",
+	types: ["Ice", "Flying"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 70,
+		atk: 115,
+		def: 59,
+		spa: 95,
+		spd: 160,
+		spe: 76
+	},
+	abilities: {
+		0: "Glacier"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+},
+cydnidie: {
+	num: -826,
+	species: "Cydnidie",
+	types: ["Bug", "Dark"],
+	baseStats: {
+		hp: 90,
+		atk: 85,
+		def: 110,
+		spa: 45,
+		spd: 100,
+		spe: 80
+	},
+	abilities: {
+		0: "Muddy Armor"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Black",
+	eggGroups: ["Undiscovered"]
+},
+name: {
+	num: -827,
+	species: "Name",
+	types: ["Bug", "Water"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 103,
+		atk: 95,
+		def: 96,
+		spa: 92,
+		spd: 87,
+		spe: 52
+	},
+	abilities: {
+		0: "Ocean",
+		2: "Endurance"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+},
+name: {
+	num: -828,
+	species: "Name",
+	types: ["Water", "Electric"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 134,
+		atk: 13,
+		def: 163,
+		spa: 7,
+		spd: 109
+		spe: 11
+	},
+	abilities: {
+		0: "Levitate"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+},
+name: {
+	num: -829,
+	species: "Name",
+	types: ["Steel", "Electric"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 91,
+		atk: 80,
+		def: 87,
+		spa: 45,
+		spd: 111,
+		spe: 19
+	},
+	abilities: {
+		0: "Levitate"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+},
+farenheat: {
+	num: -830,
+	species: "Farenheat",
+	types: ["Water", "Fire"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 73,
+		atk: 66,
+		def: 63,
+		spa: 103,
+		spd: 63,
+		spe: 112
+	},
+	abilities: {
+		0: "Boiling Point"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+},
+celsice: {
+	num: -831,
+	species: "Celsice",
+	types: ["Ice"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		def: 73,
+		atk: 112,
+		def: 63,
+		spa: 66,
+		spd: 63,
+		spe: 103
+	},
+	abilities: {
+		0: "Freezing Point"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+},
+spychic: {
+	num: -833,
+	species: "Spychic",
+	types: ["Dark", "Psychic"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 60,
+		atk: 50,
+		def: 70,
+		spa: 100,
+		spd: 70,
+		spe: 160
+	},
+	abilities: {
+		0: "Superweak Armor"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+},
+murdoom: {
+	num: -834,
+	species: "Murdoom",
+	types: ["Dark", "Fighting"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 80,
+		atk: 140,
+		def: 90,
+		spa: 65,
+		spd: 80,
+		spe: 105
+	},
+	abilities: {
+		0: "Relentless"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Black",
+	eggGroups: ["Undiscovered"]
+},
+name: {
+	num: -836,
+	species: "Name",
+	types: ["Bug", "Dragon"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 81,
+		atk: 110,
+		def: 80,
+		spa: 30,
+		spd: 80,
+		spe: 99
+	},
+	abilities: {
+		0: "Levitate",
+		H: "Sharp Shooter"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]	
+},
+name: {
+	num: -837,
+	species: "Name",
+	types: ["Steel", "Dragon"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 107,
+		atk: 125,
+		def: 77,
+		spa: 52,
+		spd: 79,
+		spe: 60
+	},
+	abilities: {
+		0: "Intimidate"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+},
+name: {
+	num: -838,
+	species: "Name",
+	types: ["Dragon", "Fighting"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 100,
+		atk: 96,
+		def: 55,
+		spa: 70,
+		spd: 55,
+		spe: 104
+	},
+	abilities: {
+		0: "Quick Blows",
+		H: "Berserk"
+	},
+	heightm: 2.3,
+	weightkg: 68.5,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+}
+,
+name: {
+	num: -839,
+	species: "Name",
+	types: ["Electric", "Ice"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5	
+	},
+	baseStats: {
+		hp: 60,
+		atk: 41,
+		def: 58,
+		spa: 117,
+		spd: 69,
+		spe: 75
+	},
+	abilities: {
+		0: "Spurred",
+		1: "Refocus",
+		H: "Overdrive"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+},
+name: {
+	num: -840,
+	species: "Name",
+	types: ["Fighting"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 111,
+		atk: 85,
+		def: 120,
+		spa: 50,
+		spd: 105,
+	 	spe: 54
+	},
+	abilities: {
+		0: "Super Slams",
+		1: "Combo Fighter",
+		H: "Sealed Armor"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+},
+name: {
+	num: -841,
+	species: "Name",
+	types: ["Dark", "Psychic"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 80,
+		atk: 80,
+		def: 80,
+		spa: 80,
+		spd: 80,
+		spe: 80
+	},
+	abilities: {
+		0: "Neutrality Barrier"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+},
+charidbyss: {
+	num: -851,
+	species: "Charidbyss",
+	types: ["Water", "Dark"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 87,
+		atk: 65,
+		def: 143,
+		spa: 105,
+		spd: 100,
+		spe: 75
+	},
+	abilities: {
+		0: "Resolve",
+		H: "Disruption"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Color",
+	eggGroups: ["Undiscovered"]
+},
+faeum: {
+	num: -855,
+	species: "Faeum",
+	types: ["Fairy", "Flying"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 90,
+		atk: 60,
+		def: 65,
+		spa: 115,
+		spd: 95,
+		spe: 110
+	},
+	abilities: {
+		0: "Mystic Veil",
+		1: "Amplify",
+		H: "Resolve"
+	},
+	heightm: 0.2,
+	weightkg: 5.5,
+	color: "White",
+	eggGroups: ["Undiscovered"]
+},
+wyvearn: {
+	num: -856,
+	species: "Wyvearn",
+	types: ["Dragon", "Flying"],
+	genderRatio: {
+		M: 0.5,
+		F: 0.5
+	},
+	baseStats: {
+		hp: 88,
+		atk: 116,
+		def: 81,
+		spa: 84,
+		spd: 89,
+		spe: 103
+	},
+	abilities: {
+		0: "Resolve",
+		1: "Tough Scales",
+		H: "Sharp Eye"
+	},
+	heightm: 0.0,
+	weightkg: 0.0,
+	color: "Green",
+	eggGroups: ["Undiscovered"]
+}
+};
