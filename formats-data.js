c2c63ab93ca27a731a9fb2bf996705f63cffd13f
Dominik Yakoubek <domyak1@nths219.org>
2015-02-24 09:19:49 -0600

    Update formats-data.js

--- formats-data.js
+++ formats-data.js
@@ -1,1 +1,10 @@
-
+simorgh: {
+	randomBattleMoves: ["concealedpower","tailslam","leveldamage","phoenixtoashes","novasword","nitrocrash","substitute","roost"],
+	isNonstandard: true,
+	tier: "BAM"
+},
+flytrap: {
+	randomBattleMoves: ["concealedpower","chlorobeam","substitute","rest","sleeptalk","flowerfang","corrode","toxify","absorbmatter"],
+	isNonstandard: true,
+	tier: "BAM"
+},
